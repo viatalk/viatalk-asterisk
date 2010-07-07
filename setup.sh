2ffac9e4fc489272d507bdf106946f416b360493
Joshua Priddle <jpriddle@nevercraft.net>
2010-07-07 00:23:02 -0400

    Whitespace

--- setup.sh
+++ setup.sh
@@ -15,4 +15,3 @@ sed -i'.bak' -e"s/{{VTSERVER}}/$VTSERVER/g" sip.conf
 sed -i'.bak' -e"s/{{VTPASSWORD}}/$VTPASSWORD/g" sip.conf
 sed -i'.bak' -e"s/{{VTNUMBER}}/$VTNUMBER/g" extensions.conf
 sed -i'.bak' -e"s/{{VTAREACODE}}/${VTNUMBER:1:3}/g" extensions.conf
-
